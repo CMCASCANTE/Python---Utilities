3584f33a181f29b68f6d6ae1890903f342f77460
Karlos <trash@lawebdekarlos.es>
2025-01-23 13:42:03 +0100

    v.1.1.4 - Basic Api Request

--- app/vistas/api.py
+++ app/vistas/api.py
@@ -36,31 +36,17 @@ def api(request):
         try:
             # GET
             if valueSelect == "GET":
-                #################
-                ## Test Values ##
-                #################
-                # url3 = "https://api.ionos.com/auth/v1/tokens/generate"
-                # url2 = "https://api.ionos.com/cloudapi/v6/datacenters"
-                # token="eyJ0eXAiOiJKV1QiLCJraWQiOiIyYzMwY2FmYi03ZjY3LTRhOWEtYjk2MS1kNjQyMDBjMjg4NTIiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJBcnN5cyIsImlhdCI6MTczNzU3MDMxNCwiY2xpZW50IjoiVVNFUiIsImlkZW50aXR5Ijp7InByaXZpbGVnZXMiOlsiREFUQV9DRU5URVJfQ1JFQVRFIiwiU05BUFNIT1RfQ1JFQVRFIiwiSVBfQkxPQ0tfUkVTRVJWRSIsIk1BTkFHRV9EQVRBUExBVEZPUk0iLCJBQ0NFU1NfQUNUSVZJVFlfTE9HIiwiUENDX0NSRUFURSIsIkFDQ0VTU19TM19PQkpFQ1RfU1RPUkFHRSIsIkJBQ0tVUF9VTklUX0NSRUFURSIsIkNSRUFURV9JTlRFUk5FVF9BQ0NFU1MiLCJLOFNfQ0xVU1RFUl9DUkVBVEUiLCJGTE9XX0xPR19DUkVBVEUiLCJBQ0NFU1NfQU5EX01BTkFHRV9NT05JVE9SSU5HIiwiQUNDRVNTX0FORF9NQU5BR0VfQ0VSVElGSUNBVEVTIiwiQUNDRVNTX0FORF9NQU5BR0VfTE9HR0lORyIsIk1BTkFHRV9EQkFBUyIsIkFDQ0VTU19BTkRfTUFOQUdFX0ROUyIsIk1BTkFHRV9SRUdJU1RSWSIsIkFDQ0VTU19BTkRfTUFOQUdFX0NETiIsIkFDQ0VTU19BTkRfTUFOQUdFX1ZQTiIsIkFDQ0VTU19BTkRfTUFOQUdFX0FQSV9HQVRFV0FZIiwiQUNDRVNTX0FORF9NQU5BR0VfTkdTIiwiQUNDRVNTX0FORF9NQU5BR0VfS0FBUyIsIkFDQ0VTU19BTkRfTUFOQUdFX05FVFdPUktfRklMRV9TVE9SQUdFIiwiQUNDRVNTX0FORF9NQU5BR0VfQUlfTU9ERUxfSFVCIiwiQ1JFQVRFX05FVFdPUktfU0VDVVJJVFlfR1JPVVBTIiwiQUNDRVNTX0FORF9NQU5BR0VfSUFNX1JFU09VUkNFUyJdLCJ1dWlkIjoiN2EyMDc5MzctNjA0Mi00Nzg1LWI4NmEtMDQ1ZjI1YTI0MWVhIiwicmVzZWxsZXJJZCI6ODI4NjI3NCwicmVnRG9tYWluIjoiaW9ub3MuZGUiLCJyb2xlIjoiYWRtaW4iLCJjb250cmFjdE51bWJlciI6MzE5Mzk1OTcsImlzUGFyZW50IjpmYWxzZX0sImV4cCI6MTc2OTEyNzkxNH0.U4_wA4t7blAHvg6bFNgAslvuJReBfS0akGkqS2ZsVL0c01rqa5pKouXxR6X2FSN2Ir2QxrzsRSUVDlSPADmhYMd_y_QvrojcZS7at6dWd6wJf_TgbMpRLmOpuDZGTnuDg-tYowzWdNSOn6ZoexyJIE99KourM0BSd819A1E-8rgS6hf_ndiJ06IHoUAbjl9boW3er14Ha_GYE0ULSonAEcaylFeABpwP-ENkyEbbIWaaefKWF7_AmvLqSX0gajrM1XPqIh3bv8I91vocgHoxrUwi74aUvJjxiu47o8cUYXSD6DllWXIBOQU-VACfBz2oJnoHnBxTEsbQYpJoH4sKtg"
-                # # Adding a payload
-                # payload = {"id": [1, 2, 3], "userId":1}
-                # response = requests.get(url, params=payload)
-
-
                 # The API endpoint
                 url = valueInput
                 # Auth Variables
                 basicHeaders = HTTPBasicAuth(basicUserValue, basicPassValue)
                 tokenAuth = {'Authorization': f'{tokenKeyValue} {tokenValueValue}'}
                 # A GET request to the API
-                if authValue == "Token":
-                    print("test3")
+                if authValue == "Token":                    
                     response = requests.get(url, headers=tokenAuth)
-                elif authValue == "Basic":
-                    print("test2")
+                elif authValue == "Basic":                    
                     response = requests.get(url, auth=basicHeaders)
-                else:
-                    print("test4")
+                else:                    
                     response = requests.get(url)
             
             # POST
@@ -80,23 +66,13 @@ def api(request):
 
                 # A POST request to the API
                 response = requests.post(url, json=data)
-
-        
-
-            respuesta = response.text
             
-
+            respuesta = response.text
         except Exception as err:
                 # print(f"Error: {err}")
                 respuesta = err
 
-
-    
-    def testing():
-        print("no java")
-    
     context = {       
-        "testing": testing,
         "respuesta": respuesta,  
         "formulario": form,
         "formularioAuth" : formAuth,
