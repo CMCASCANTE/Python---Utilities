a160787f83262743d6f548219787e1803946d6c5
Karlos <trash@lawebdekarlos.es>
2025-01-23 16:35:54 +0100

    v1.1.6 - GET Done

--- app/vistas/formularios/forms.py
+++ app/vistas/formularios/forms.py
@@ -2,6 +2,9 @@ from django import forms
 from django.core.exceptions import ValidationError
 
 
+##############################################
+# Formulario para la función de buscador DNS #
+##############################################
 class DNSForm(forms.Form):
     # Input
     domainValue = forms.CharField(label="", max_length=200, required=False, initial=None, widget=forms.TextInput(attrs={'placeholder': 'Introduce un dominio'}))
@@ -22,8 +25,6 @@ class DNSForm(forms.Form):
     ) 
     selectDNS = forms.ChoiceField(label="", choices = TYPE_CHOICES, initial = "A")  
 
-
-
     # Select    
     NAMESERVER_CHOICES =(          
         ("8.8.8.8", "Google"),
@@ -36,22 +37,21 @@ class DNSForm(forms.Form):
     # Añadimos un valor al atributo clase para darle CSS
     nameServer.widget.attrs['class'] = "nameServerInput"
 
-
-
-
-
+######################################################
+# Formulario para la función de Testeador de Puertos #
+######################################################
 class PortForm(forms.Form):
     # Input
     ipValue = forms.GenericIPAddressField(label="", protocol="IPv4", max_length=15, required=False, initial=None, widget=forms.TextInput(attrs={'placeholder': 'IP'}))
     portValue = forms.CharField(label="", initial=None, max_length=5, required=False, widget=forms.TextInput(attrs={'placeholder': 'Puerto', 'class': 'number'}))
     #portValue = forms.DecimalField(label="", min_value=1, max_value=65535, decimal_places=0, initial=None, widget=forms.NumberInput(attrs={'placeholder': 'Puerto'}))
 
-
-
-
+#################################################
+# Formularios para la función de Peticiones API #
+#################################################
 class ApiForm(forms.Form):
     # Input
-    urlValue = forms.CharField(label="", max_length=2000, required=True, initial=None, widget=forms.TextInput(attrs={'placeholder': 'API Endpoint'}))
+    urlValue = forms.CharField(label="", max_length=2000, required=False, initial=None, widget=forms.TextInput(attrs={'placeholder': 'API Endpoint', 'autocomplete': "off"}))
     
     # Select    
     TYPE_CHOICES =( 
@@ -59,8 +59,8 @@ class ApiForm(forms.Form):
         ("POST", "POST")        
     ) 
     apiRequestType = forms.ChoiceField(label="", choices = TYPE_CHOICES, initial = "GET", widget=forms.Select(attrs={'disabled': ''}))  
-class ApiFormAuth(forms.Form):
 
+class ApiFormAuth(forms.Form):
     # Select    
     TYPE_CHOICES =( 
         ("None", "None"), 
@@ -68,12 +68,13 @@ class ApiFormAuth(forms.Form):
         ("Token", "Token")        
     ) 
     apiAuthType = forms.ChoiceField(label="Auth Type", choices = TYPE_CHOICES, initial = "", widget=forms.Select(attrs={'onchange':'authHidden(this)'}))  
+
 class ApiFormAuthBasic(forms.Form):
     # Auth
-    basicUser = forms.CharField(label="", max_length=200, required=False, initial=None, widget=forms.TextInput(attrs={'placeholder': 'User'}))
+    basicUser = forms.CharField(label="", max_length=200, required=False, initial=None, widget=forms.TextInput(attrs={'placeholder': 'User', 'autocomplete': "off"}))
     basicPass = forms.CharField(label="", max_length=200, required=False, initial=None, widget=forms.PasswordInput(attrs={'placeholder': 'Pass', 'autocomplete': "off"}))
     
 class ApiFormAuthToken(forms.Form):
     # Auth
-    tokenKey = forms.CharField(label="", max_length=200, required=False, initial=None, widget=forms.TextInput(attrs={'placeholder': 'Key'}))
-    tokenValue = forms.CharField(label="", max_length=20000, required=False, initial=None, widget=forms.TextInput(attrs={'placeholder': 'Token'}))
+    tokenKey = forms.CharField(label="", max_length=200, required=False, initial=None, widget=forms.TextInput(attrs={'placeholder': 'Key', 'autocomplete': "off"}))
+    tokenValue = forms.CharField(label="", max_length=20000, required=False, initial=None, widget=forms.TextInput(attrs={'placeholder': 'Token', 'autocomplete': "off"}))
